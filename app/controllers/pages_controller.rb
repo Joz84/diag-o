565cf2b65067a9be0403697b333cfa27c2780710
jules maregiano <jules.maregiano@gmail.com>
2017-09-07 20:22:29 +0200

    Transforme une zone en array. Difficultés à les pousser vers la vue pour faire la méthode polygone dessus

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,14 +10,17 @@ class PagesController < ApplicationController
   end
 
   def eligibility
-    @color = "blue"
-    @area_orange = [
-     { lat: 44.80359712236584, lng: -0.51317632796123 },
-     { lat: 44.80343112410052,  lng:-0.517242017073627 },
-     { lat: 44.803432239850856, lng:-0.517244698569879 },
-     { lat: 44.803432923191316,  lng:-0.517242247997421 },
-     { lat: 44.7917,  lng:-0.5161 },
-     { lat: 44.7928,  lng:-0.5007 },
-    ];
+    @array = []
+    @ZoneTest = Zone.first.points.each do |p|
+      h = {}
+      h['lat'] = p.lat
+      h['lng'] = p.lng
+      @array << h
+    end
   end
 end
+
+
+
+
+
