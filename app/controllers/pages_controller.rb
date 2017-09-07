6420e14f4f28498b3b20c262e47f57e6ed7a655f
Maxime Boue <abriko@yahoo.fr>
2017-09-07 10:22:46 +0200

    Add skip on controller elligibility

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :eligibility]
 
   def home
+  end
+
+  def eligibility
+
   end
 end
