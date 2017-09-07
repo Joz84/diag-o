c38be68a7fb60aea73716bee7ef6f5cd84ffeedc
Maxime Boue <abriko@yahoo.fr>
2017-09-07 10:45:39 +0200

    Do link between home and eligibility

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :eligibility]
 
   def home
+
   end
 
   def eligibility
-
+    @address = params[:query][:address]
   end
 end
