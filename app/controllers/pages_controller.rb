e3bcb1ba3f6f904e05eb7fd08d106c66dfdbd368
Maxime Boue <abriko@yahoo.fr>
2017-09-08 16:02:08 +0200

    Work on map, autocomplete suck

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,34 +6,17 @@ class PagesController < ApplicationController
   end
 
   def eligibility
-    @address = params[:query][:address]
-  end
 
-  def eligibility
-    # @zones = Zone.all
-    # @zones.each_with_index do |zone, index|
+    @address = params[:query][:address]
+
     @town = Town.first
     @zoneslist = @town.zones.map do |zone|
       zoneinfos = []
       zoneinfos << zone.points.map do |point|
         {'lat' => point.lat.to_f, 'lng' => point.lng.to_f}
       end
-      zoneinfos << ['#00FF00', '#FFA500', '#0000FF'].sample
+      zoneinfos << zone.color
       zoneinfos
     end
   end
 end
-
-
-
-
-# i=0
-# Zone.all.each_with_index do |zone, index|
-#   @pointslist = []
-#   i +=1
-#   puts "ZONE + #{i}"
-#   zone.points.each do |point|
-#     @pointslist << point
-#   end
-#     puts @pointslist.size
-# end
