a73ac59bde1aaaa45350dfd854322159f95d04c9
Maxime Boue <abriko@yahoo.fr>
2017-09-08 14:44:04 +0200

    Rename colore to color in Zone model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908094117) do
+ActiveRecord::Schema.define(version: 20170908124235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 20170908094117) do
 
   create_table "zones", force: :cascade do |t|
     t.bigint "town_id"
-    t.string "colore"
+    t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "id_zone"
