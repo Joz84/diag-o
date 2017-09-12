0f229368f123aefa188428f2a85a1b91cd058890
jules maregiano <jules.maregiano@gmail.com>
2017-09-12 11:36:05 +0200

    Ajout de la gem pundit. Besoin d'avoir des CRUDs complets avant de pouvoir commencer à donner des authorizations.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
-  
+
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+
   def set_locale
     I18n.locale = :fr
   end
@@ -27,4 +33,18 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: user_data)
       devise_parameter_sanitizer.permit(:account_update, keys: user_data)
   end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
 end
