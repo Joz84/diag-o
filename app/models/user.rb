2b5ffb43c7b8ed048f131da87512103e1185a91c
Maxime Boue <abriko@yahoo.fr>
2017-09-20 16:20:10 +0200

    Adding role to user, field not valid on new registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, presence: :true
+  validates :last_name, presence: :true
+  validates :phone, presence: :true
+  validates :role, presence: :true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookings
