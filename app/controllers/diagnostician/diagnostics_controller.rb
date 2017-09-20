f0539b6fbc427d15250dd07b881cf0ab263bef5b
Maxime Boue <abriko@yahoo.fr>
2017-09-20 22:36:48 +0200

    Add Edit profile on sidebar and view

--- app/controllers/diagnostician/diagnostics_controller.rb
+++ app/controllers/diagnostician/diagnostics_controller.rb
@@ -2,8 +2,13 @@ class Diagnostician::DiagnosticsController < ApplicationController
   before_action :params_user, only: [:index, :show]
 
   def index
-    @diagnostics = Diagnostic.all # car un seul diagnosticien pour l'instant
-    @housings = Housing.all
+    @user = current_user
+    if @user.diagnostician?
+      @diagnostics = Diagnostic.all # car un seul diagnosticien pour l'instant
+      @housings = Housing.all
+    else
+      @diagnostics =
+    end
   end
 
   def show
