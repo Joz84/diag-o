57333d502848e9db244201eef086c7d28213523f
Maxime Boue <abriko@yahoo.fr>
2017-10-12 12:28:33 +0200

    Add confirm and cancel booking

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get '/confirmation', to: 'inscriptions#confirmation'
   get "diagnostician/diagnostic/:id/add_plan", to: "diagnostician/diagnostics#add_plan", as: "add_plan"
   get "diagnostician/diagnostic/:id/delete_plan", to: "diagnostician/diagnostics#delete_plan", as: "delete_plan"
+  get "diagnostician/booking/:id/add_comment", to: "diagnostician/bookings#add_comment", as: "add_comment"
+  get "diagnostician/booking/:id/delete_comment", to: "diagnostician/bookings#delete_comment", as: "delete_comment"
 
   resources :users, only: [:show]
   resources :answers
