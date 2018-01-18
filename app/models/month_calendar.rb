12efc23314ff24577464512631b4250014dd621d
Powakedown <abriko@yahoo.fr>
2018-01-18 09:02:42 +0100

    Delete month_calendar model to fix agenda on heroku

--- app/models/month_calendar.rb
+++
@@ -1,9 +0,0 @@
-module SimpleCalendar
-  class MonthCalendar < SimpleCalendar::Calendar
-    private
-    def date_range
-      week_start_day = :monday
-      beginning_of_week = start_date.beginning_of_month.beginning_of_week(week_start_day)
-    end
-  end
-end
